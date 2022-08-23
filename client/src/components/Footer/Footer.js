09a58a185aab96435828db4ee4d5c680c624471e
kyoh2014 <kyoh2014@gmail.com>
2022-08-23 16:04:39 -0300

    Added footer component

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,12 +1,23 @@
 import React from "react";
 
+const url = {
+    LinkedIn: "https://www.linkedin.com/in/daniel-gutierrez-460a8417b/",
+    GitHub: "https://github.com/kyoh2014"
+}
+export default function Footer() {
+  const handleClick = (e) => {
+    e.preventDefault();
+    window.open(url[e.target.name], "_blank")
+  };
 
-export default function Footer(){
-
-    return(
-        <div>
-            <h1>Footer</h1>
-        </div>
-    )
-
+  return (
+    <div>
+      <button onClick={handleClick}>
+        <img name="LinkedIn" src="/icons/LinkedinIcon.png" alt="image"/>
+      </button>
+      <button onClick={handleClick}>
+        <img name="GitHub" src="/icons/GitHubIcon.png" alt="image"/>
+      </button>
+    </div>
+  );
 }
