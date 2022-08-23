5c17b9a9f7c7e2b68868c9ea6b0fc60793ca1629
kyoh2014 <kyoh2014@gmail.com>
2022-08-23 16:05:49 -0300

    Added navbar component

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+export default function Navbar({ token, setToken }) {
+
+  const navigate = useNavigate()
+  
+  const handleLogOut = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("token")
+    setToken(null)
+    navigate("/")
+  };
 
 
-export default function Navbar(){
-
-    return(
-        <div>
-            <h1>Navbar</h1>
-        </div>
-    )
-
+  return (
+    <>
+      <nav>
+        <NavLink to={token === null ? "/" : "/home"}>MyBudget</NavLink>
+        {token === null ? (
+          <>
+            <NavLink to="/auth">Login</NavLink>
+            <NavLink to="/auth/register">Register</NavLink>
+          </>
+        ) : (
+          <NavLink to="/" onClick={handleLogOut}>
+            Logout
+          </NavLink>
+        )}
+      </nav>
+    </>
+  );
 }
