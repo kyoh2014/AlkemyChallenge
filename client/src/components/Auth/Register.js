f6d7a951e304a1865958aa98f70c3eb510e9462d
kyoh2014 <kyoh2014@gmail.com>
2022-08-23 16:08:08 -0300

    Added register component

--- client/src/components/Auth/Register.js
+++ client/src/components/Auth/Register.js
@@ -1,12 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { REGISTER_URL } from "../../configs/api_url";
 
+export default function Register() {
+ 
+  const navigate = useNavigate();
+  
+  const [form, setForm] = useState({
+    name: "",
+    lastname: "",
+    password: "",
+    username: "",
+    email: "",
+  });
 
-export default function Register(){
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(REGISTER_URL, {
+      method: "post",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(form)
+      
+    })
+    if(response.status === 201) {
+    navigate("/auth") 
+    }
+  } catch (e) {
+    console.error(e)
+  }
+}
 
-    return(
-        <div>
-            <h1>Register</h1>
+  return (
+    <div>
+      <h1>Register</h1>
+      <form onSubmit={handleSubmit}>
+      <div className="">
+          <label>Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            onChange={handleChange}
+          />
         </div>
-    )
-
+        <div className="">
+          <label>Lastname</label>
+          <input
+            type="text"
+            id="lastname"
+            name="lastname"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="">
+          <label>Username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="">
+          <label>E-mail</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="">
+          <label>Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            onChange={handleChange}
+          />
+        </div>
+        
+        <button type="submit">
+          Register
+        </button>
+      </form>
+    </div>
+  );
 }
