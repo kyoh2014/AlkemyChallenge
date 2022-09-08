a6fd391d316c75e93fd9d0fe913f7ae0a0076f8c
kyoh2014 <kyoh2014@gmail.com>
2022-09-08 17:43:44 -0300

    Minor fixes to categories controller

--- api/src/controllers/categoriesController.js
+++ api/src/controllers/categoriesController.js
@@ -50,24 +50,6 @@ const searchByIdController = async(req, res) => {
 
 const updateController = async(req, res) => {
 
-    // if (Category.deletedAt) {
-    //     Category.update({
-    //         deletedAt: null
-    // }, {
-    //         where: {
-    //             id: req.params.id
-    //         }
-    //     })
-    //     .then((result) => {
-    //         res.status(201).send(result);
-    //     })
-    //     .catch(err => {
-    //         res.status(400).send({
-    //             error:err
-    //         });
-    //     })
-    // }
-
     Category.update({
         name: req.body.name,
         description: req.body.description,
