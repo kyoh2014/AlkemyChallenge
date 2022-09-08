47fc8c9cef6f47ce01c9c43410d9bd20fee554cd
kyoh2014 <kyoh2014@gmail.com>
2022-09-08 17:28:48 -0300

    Recentlist component in userhome implemented

--- client/src/components/UserHome/RecentList.js
+++ client/src/components/UserHome/RecentList.js
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { RECENTLIST_URL } from "../../configs/api_url";
+import OperationItem from "../Utils/OperationItem";
 
+export default function RecentList({}) {
+  const [recentList, setRecentList] = useState(undefined);
+  const [token, setToken] = useState(localStorage.getItem("token"));
 
-export default function RecentList(){
+  useEffect(() => {
+    const fetchBalance = async () => {
+      try {
+        const response = await fetch(RECENTLIST_URL, {
+          method: "get",
+          headers: {
+            "Content-Type": "application/json",
+            "authorization": `bearer ${token}`,
+          },
+        });
+        const data = await response.json();
 
-    return(
-        <div>
-            <h1>RecentList</h1>
-        </div>
-    )
+        if (response.status === 200) {
+          setRecentList(data.data);
+        } else {
+          setToken(null);
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    fetchBalance();
+  }, [token]);
 
+  return (
+    <div>
+      <h1>Your 10 most recent transactions</h1>
+      {recentList?.map((transaction) => (
+          <OperationItem
+            key={transaction.id}
+            id={transaction.id}
+            amount={transaction.amount}
+            concept={transaction.concept}
+            date={transaction.date}
+            type={transaction.type}
+            category={transaction.Category.name}
+          />
+      ))}
+    </div>
+  );
 }
