0a91d3a02328103b937d87c2e5301000f01711f3
kyoh2014 <kyoh2014@gmail.com>
2022-09-08 17:15:23 -0300

    Implemented home component

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 
 
-export default function Home(){
+export default function Home({token}) {
 
-    return(
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
+  const navigate = useNavigate()
 
+  useEffect(()=>{
+    console.log(token)
+    if(token){
+      navigate("/home")
+    }
+  })
+
+  return (
+    <div>
+      <h1>MyBudget</h1>
+      <p>
+        Keep track of your expenses and your income. Get Started!
+        <NavLink to="/auth/register">
+            Start
+        </NavLink>
+      </p>
+    </div>
+  )
 }
