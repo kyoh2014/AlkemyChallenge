84d1a163db1f3342add912ae93acc17e037a3492
kyoh2014 <kyoh2014@gmail.com>
2022-09-08 17:43:26 -0300

    Minor fixes to balance controller

--- api/src/controllers/balanceController.js
+++ api/src/controllers/balanceController.js
@@ -1,49 +1,72 @@
-const { Operation, Category, sequelize }= require('../models/index');
-const { OPERATIONS_IN_RESUME } = require('../configs/index')
+const { Op } = require("sequelize");
+const { Operation, Category, sequelize } = require("../models/index");
+const { OPERATIONS_IN_RESUME } = require("../configs/index");
 
-const balanceController = async(req, res) => {
-
-    Operation.findAll({
-        where: {
-            deletedAt: null,
-            idUser: req.user.id
-        },
-        attributes: [[sequelize.fn('sum', sequelize.col('amount')), 'total']]
-        
-    })
-    .then(totalAmount => {
-
-        res.json({ data: totalAmount })
-
-    })
-
+const balanceController = async (req, res) => {
+  Operation.findAll({
+    where: {
+      deletedAt: null,
+      idUser: req.user.id,
+    },
+    attributes: [[sequelize.fn("sum", sequelize.col("amount")), "total"]],
+  }).then((totalAmount) => {
+    if (totalAmount.total === null) {
+      res.json({ data: 0 });
+    } else {
+        res.json({ data: totalAmount });
+    }
+});
 };
 
-const resumeController = async(req, res) => {
+const resumeController = async (req, res) => {
+  Operation.findAll({
+    where: {
+      
+      deletedAt: null,
+      idUser: req.user.id,
+    },
+    include: {
+      model: Category,
+      attributes: ["name"],
+    },
+    attributes: ["id", "concept", "amount", "date", "type"],
 
-    Operation.findAll({
-        where: {
-            deletedAt:null,
-        },
-        include: {
-            model: Category,
-            attributes: ['name']
-        },
-        attributes: ['id', 'concept', 'amount', 'date', 'type'],
+    order: [["date", "DESC"]],
+    limit: OPERATIONS_IN_RESUME,
+  }).then((amount) => {
+    res.json({ data: amount });
+  });
+};
 
-        order: [
-            ['id', 'DESC']
-        ],
-        limit: OPERATIONS_IN_RESUME
-        
-    })
-    .then(amount => {
+const allResumeController = async (req, res) => {
 
-        res.json({ data: amount })
+  let type = req.query.type
 
-    })
+    if(!(type.toLowerCase() === "income" || type.toLowerCase() === "egress" || type === undefined)){
+        return res.status(400).json({msg: `The type filter is invalid`})
+    } else if(type === undefined){
+        type = ""
+    }
 
-}
+  Operation.findAll({
+    where: {
+      type:{
+        [Op.like]: `%${type}%`
+    },
+      deletedAt: null,
+      idUser: req.user.id,
+    },
+    include: {
+      model: Category,
+      attributes: ["name"],
+    },
+    attributes: ["id", "concept", "amount", "date", "type"],
 
+    order: [["date", "DESC"]],
 
-module.exports = { balanceController, resumeController };
+  }).then((allamount) => {
+    res.json({ data: allamount });
+  });
+};
+
+module.exports = { balanceController, resumeController, allResumeController };
