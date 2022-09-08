eae09d6cac5f88889f1d04def92994c2009d07f7
kyoh2014 <kyoh2014@gmail.com>
2022-09-08 17:28:05 -0300

    Balance component in userhome implemented

--- client/src/components/UserHome/Balance.js
+++ client/src/components/UserHome/Balance.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { BALANCE_URL } from "../../configs/api_url";
 
+export default function Balance({}) {
+  const [balance, setBalance] = useState(undefined);
+  const [token, setToken] = useState(localStorage.getItem("token"));
 
-export default function Balance(){
+  useEffect(() => {
+    const fetchBalance = async () => {
+      try {
+        const response = await fetch(BALANCE_URL, {
+            method: "get",
+            headers: {
+              'Content-Type': 'application/json', 
+              'authorization': `bearer ${token}`
+            }
+        });
+        const data = await response.json();
 
-    return(
-        <div>
-            <h1>balance</h1>
-        </div>
-    )
+        if (response.status === 200) {
+          setBalance(data.data[0].total);
+        } else {
+            setToken(null);
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    fetchBalance()
+  }, []);
 
+  return (
+    <div>
+      <h1>Your current balance is{" "}
+      <span className="">${balance}</span>
+      </h1>
+    </div>
+  );
 }
