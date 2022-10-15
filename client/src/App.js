98b9e63cd465826bd4e9e492849c8c2e467dfcd2
kyoh2014 <kyoh2014@gmail.com>
2022-10-15 16:36:13 -0300

    CSS implementation.

--- client/src/App.js
+++ client/src/App.js
@@ -1,69 +1,51 @@
 import "./App.css"
-import { BrowserRouter as Router, Route, Routes, redirecTo } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 import { ISAUTHENTICATE_URL } from "./configs/api_url"
-
 import ProtectedRoute from "./components/Utils/ProtectedRoute"
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer"
 import Home from "./components/Home/Home";
-import Register from "./components/Auth/Register";
-import Login from "./components/Auth/Login";
 import UserHome from "./components/UserHome/UserHome";
-import Transaction from "./components/Transactions/Transaction";
 import TransactionList from "./components/Transactions/TransactionList";
-import TransactionEdit from "./components/Transactions/TransactionEdit";
-import TransactionDelete from "./components/Transactions/TransactionDelete";
-
-
 
 
 function App() {
   const [token, setToken] = useState(localStorage.getItem("token"));
-
+  const [buttonRegister, setButtonRegister] = useState(false)
+ 
   useEffect(() => {
-  const fetchData = async() =>{
-    try {
-      const response = await fetch(ISAUTHENTICATE_URL, {
-      method: "get",
-      headers: {
-        'Content-Type': 'application/json', 
-        'authorization': `bearer ${token}`
+    const fetchData = async() =>{
+      try {
+        const response = await fetch(ISAUTHENTICATE_URL, {
+          method: "get",
+          headers: {
+            'Content-Type': 'application/json', 
+            'authorization': `bearer ${token}`
+          }
+        })
+        if(response.status !== 200) {
+          setToken(null);
+        }
+      } catch (e) {
+        console.error(e)
       }
-    })
-    if(response.status !== 200) {
-      setToken(null);
     }
-  } catch (e) {
-    console.error(e)
-  }}
-  fetchData();
-}, [token]);
+    fetchData();
+  }, [token]);
 
   return (
+  <div className="App">
     <Router>
-      <div className="App">
-        <Navbar token={token} setToken={setToken}/>
+      <Navbar token={token} setToken={setToken} buttonRegister={buttonRegister} setButtonRegister={setButtonRegister}/>
         <Routes>
-          <Route path="/" element={<Home token={token}/>} />
-
-          <Route path="/auth/register" element={<Register />} />
-
-          <Route path="/auth" element={<Login setToken={setToken}/>} />
-
+          <Route path="/" element={<Home setButtonRegister={setButtonRegister} token={token}/>} />
           <Route path="/home" element={<ProtectedRoute token={token}><UserHome token={token}/></ProtectedRoute>} />
-
-          <Route path="/transaction" element={<ProtectedRoute token={token}><Transaction token={token}/></ProtectedRoute>} />
-
-          <Route path="/transaction/list" element={<ProtectedRoute token={token}><TransactionList token={token}/></ProtectedRoute>} />
-
-          <Route path="/transaction/edit/:id" element={<ProtectedRoute token={token}><TransactionEdit token={token}/></ProtectedRoute>} />
-          
-          <Route path="/transaction/delete/:id" element={<ProtectedRoute token={token}><TransactionDelete token={token}/></ProtectedRoute>} />
+          <Route path="/transactions" element={<ProtectedRoute token={token}><TransactionList token={token}/></ProtectedRoute>} />
         </Routes>
-        <Footer />
-      </div>
+      <Footer />
     </Router>
+  </div>
   );
 }
 
