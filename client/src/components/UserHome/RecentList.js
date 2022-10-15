4d3ef261f573158e499821de13e2d9cf686bf3be
kyoh2014 <kyoh2014@gmail.com>
2022-10-15 16:45:12 -0300

    CSS implementation.

--- client/src/components/UserHome/RecentList.js
+++ client/src/components/UserHome/RecentList.js
@@ -1,8 +1,9 @@
+import "./RecentList.css";
 import React, { useEffect, useState } from "react";
 import { RECENTLIST_URL } from "../../configs/api_url";
 import OperationItem from "../Utils/OperationItem";
 
-export default function RecentList({token}) {
+export default function RecentList({ token }) {
   const [recentList, setRecentList] = useState(undefined);
 
   useEffect(() => {
@@ -16,10 +17,10 @@ export default function RecentList({token}) {
           },
         });
         const data = await response.json();
-
+        
         if (response.status === 200) {
           setRecentList(data.data);
-        } 
+        }
       } catch (e) {
         console.error(e);
       }
@@ -28,19 +29,31 @@ export default function RecentList({token}) {
   }, [token]);
 
   return (
-    <div>
-      <h1>Your 10 most recent transactions</h1>
-      {recentList?.map((transaction) => (
-          <OperationItem
-            key={transaction.id}
-            id={transaction.id}
-            amount={transaction.amount}
-            concept={transaction.concept}
-            date={transaction.date}
-            type={transaction.type}
-            category={transaction.Category.name}
-          />
-      ))}
+    <div className="recentlist_main">
+      <div className="recentlist_title">
+        <h1>The 10 most recent transactions</h1>
+      </div>
+      <div className="recentlist_container">
+        {recentList &&
+          (recentList.length === 0 ? (
+            <div className="recentlist_container_empty">
+              <h1>Your transactions will be here.</h1>
+            </div>
+          ) : (
+            recentList?.map((transaction) => (
+              <OperationItem
+                token={token}
+                key={transaction.id}
+                id={transaction.id}
+                amount={transaction.amount}
+                concept={transaction.concept}
+                date={transaction.date}
+                type={transaction.type}
+                category={transaction.Category.name}
+              />
+            ))
+          ))}
+      </div>
     </div>
   );
 }
