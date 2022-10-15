d5eabbb88a576ae451f31c7f9bd3252f5d9c76e9
kyoh2014 <kyoh2014@gmail.com>
2022-10-15 16:37:55 -0300

    Visual improvement of the start of Home and implementation of CSS

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,8 +1,8 @@
+import "./Home.css"
 import React, { useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-
-export default function Home({token}) {
+export default function Home({token, setButtonRegister}) {
 
   const navigate = useNavigate()
 
@@ -12,15 +12,28 @@ export default function Home({token}) {
     }
   })
 
+  const handleButton = (e) =>{
+    setButtonRegister(true)
+  }
+
   return (
-    <div>
-      <h1>MyBudget</h1>
-      <p>
-        Keep track of your expenses and your income. Get Started!
-        <Link to="/auth/register">
-            Start
-        </Link>
+    <div className="home_main">
+      <div className="home_title">
+        <h1>Keep track of your expenses and your income. Get Started!</h1>
+      </div>
+      <div className="home_description">
+      <p className="home_description_margin">
+        Record your money movements and check the total balance.
       </p>
+      <p className="home_description_margin">
+        You will be able to keep track of the expenses and income you make and have them well categorized.
+      </p>
+      </div>
+      <div className="home_start">
+          <button className="button" onClick={handleButton}>
+            Get Started
+          </button>
+      </div>      
     </div>
   )
 }
