249ce63dd4b4ac9ba61249a27b0907bf593b5191
kyoh2014 <kyoh2014@gmail.com>
2022-10-15 16:53:21 -0300

    Minor fixes and CSS implementation.

--- client/src/components/Transactions/TransactionList.js
+++ client/src/components/Transactions/TransactionList.js
@@ -1,13 +1,18 @@
+import "./TransactionList.css"
 import React, { useEffect, useState } from "react";
 import { ALLRECENTLIST_URL,
   CATEGORIES_URL } from "../../configs/api_url";
 import OperationItem from "../Utils/OperationItem";
 
 export default function RecentList({ token }) {
-  const [type, setType] = useState(undefined);
-  const [idCategory, setIdCategory] = useState(undefined);
   const [listCategory, setListCategory] = useState([])
   const [transactionList, setTransactionList] = useState([]);
+  const [type, setType] = useState({
+    type: "",
+  });
+  const [idCategory, setIdCategory] = useState({
+    idCategory: "",
+  });
 
   useEffect(() => {
     const fetchList = async () => {
@@ -66,44 +71,60 @@ export default function RecentList({ token }) {
   };
 
   return (
-    <div>
-      <h1>Your Transactions</h1>
+    <div className="transactionlist_main">
+      <div className="transactionlist_title">
+        <h1>All your Transactions</h1>
+      </div>
       <form onSubmit={handleSubmit}>
-        <div className="">
-          <label>Type</label>
+       <div className="transactionlist_select">
+        <div className="transactionlist_type">
+          <label>Type: </label>
           <select type="text" id="type" name="type" onChange={handleChangeType}>
-            <option value="">Select Type Of Transaction</option>
+            <option value="" disabled>Select Type Of Transaction</option>
+            <option value="">All Transactions</option>
             <option value="income">Income</option>
             <option value="egress">Egress</option>
           </select>
         </div>
-        <div className="">
-        <label>Category</label>
+        <div className="transactionlist_category">
+        <label>Category: </label>
         <select
             type="text"
             id="idCategory"
             name="idCategory"
             onChange={handleChangeCategory}
             >
-            <option value="">Select Category Of Transaction</option>
+            <option value="" disabled>Select Category Of Transaction</option>
+            <option value="">All Transactions</option>
             {listCategory?.map((category) => (
-        <option value={category.id} key={category.id}> {category.name}</option>
-      ))}
+              <option value={category.id} key={category.id}> {category.name}</option>
+            ))}
           </select>
+          </div>
+        <button className="button" type="submit">Select</button>
         </div>
-        <button type="submit">Select</button>
-      </form>
-      {transactionList?.map((transaction) => (
-        <OperationItem
-          key={transaction.id}
-          id={transaction.id}
-          amount={transaction.amount}
-          concept={transaction.concept}
-          date={transaction.date}
-          type={transaction.type}
-          category={transaction.Category.name}
-        />
-      ))}
+        </form>
+        <div className="transactionlist_container">
+        {transactionList && (transactionList.length === 0 ? (
+          <div className="transactionlist_container_empty">
+            <h1>The transaction log will be displayed here.</h1>
+          </div>
+          ) : (
+            transactionList?.map((transactionList) => (
+              <div className="transactionlist_form">
+                <OperationItem
+                  token={token}
+                  key={transactionList.id}
+                  id={transactionList.id}
+                  amount={transactionList.amount}
+                  concept={transactionList.concept}
+                  date={transactionList.date}
+                  type={transactionList.type}
+                  category={transactionList.Category.name}
+                />
+              </div>
+          ))))}
+      </div>
     </div>
   );
 }
@@ -122,10 +143,7 @@ function generateQuery(type, category) {
     query += '&'
     flag = true;
   } 
-
-
   if(!flag) return query;
 
   return "?" + query.slice(0, -1)
-
 }
